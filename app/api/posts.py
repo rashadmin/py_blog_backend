b406f7955328ebbdd5ac71bd11275499e1b66676
rashadmin <abdul.a.rasheed2021@gmail.com>
2025-01-15 14:17:44 +0100

    pheeeew did lots of stuff today from creating route for the post api end point to creating the authn and authz i.e authentication and authorizarion for each route, to adding few tables in the user and post table like token, token expiration date,description and status... might look basic but trust me it's crazyyyyyyyyyyyyyyyyyyyyyyyy lol

--- app/api/posts.py
+++ app/api/posts.py
@@ -1,10 +1,42 @@
 from app.api import bp
-from flask import jsonify,request
-from app.models import Post
+from flask import jsonify,request,abort
+from app.models import Post,User
+from app.api.auth import token_auth
 
 @bp.route('/posts', methods=['GET'])
+@token_auth.login_required
 def get_posts():
     page = request.args.get('page', 1, type=int)
-    per_page = min(request.args.get('per_page', 2, type=int), 100)
+    per_page = min(request.args.get('per_page', 10, type=int), 100)
     data = Post.to_collection_dict(Post.query, page, per_page, 'api.get_posts')
     return jsonify(data)
+
+
+@bp.route('/users/<int:id>/posts', methods=['GET'])
+@token_auth.login_required
+def get_user_posts(id):
+    if token_auth.current_user().id != id:
+        abort(403)
+    page = request.args.get('page', 1, type=int)
+    per_page = min(request.args.get('per_page', 4, type=int), 100)
+    posts = User.query.get_or_404(id).posts
+    data = Post.to_collection_dict(posts, page, per_page, 'api.get_posts')
+    return jsonify(data)
+
+
+@bp.route('/users/<int:id>', methods=['PUT'])
+@token_auth.login_required
+def update_user(id):
+    if token_auth.current_user().id != id:
+        abort(403)
+    user = User.query.get_or_404(id)
+    data = request.get_json() or {}
+    if 'username' in data and data['username'] != user.username and \
+            User.query.filter_by(username=data['username']).first():
+        return bad_request('please use a different username')
+    if 'email' in data and data['email'] != user.email and \
+            User.query.filter_by(email=data['email']).first():
+        return bad_request('please use a different email address')
+    user.from_dict(data, new_user=False)
+    db.session.commit()
+    return jsonify(user.to_dict())
--- app/models.py
+++ app/models.py
@@ -1,8 +1,11 @@
 from app import db
-from datetime import datetime
+import base64
+from datetime import datetime, timedelta
+import os
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 from app import login
+
 from hashlib import md5
 from flask import url_for
 
@@ -34,11 +37,14 @@ class PaginatedAPIMixin(object):
 
 
 
-class User(UserMixin,db.Model):
+class User(UserMixin,PaginatedAPIMixin,db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    token = db.Column(db.String(32), index=True, unique=True)
+    token_expiration = db.Column(db.DateTime)
+
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -54,6 +60,25 @@ class User(UserMixin,db.Model):
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
+
+    def get_token(self, expires_in=3600):
+        now = datetime.now()
+        if self.token and self.token_expiration > now + timedelta(seconds=60):
+            return self.token
+        self.token = base64.b64encode(os.urandom(24)).decode('utf-8')
+        self.token_expiration = now + timedelta(seconds=expires_in)
+        db.session.add(self)
+        return self.token
+
+    def revoke_token(self):
+        self.token_expiration = datetime.now() - timedelta(seconds=1)
+
+    @staticmethod
+    def check_token(token):
+        user = User.query.filter_by(token=token).first()
+        if user is None or user.token_expiration < datetime.now():
+            return None
+        return user
     
     def to_dict(self, include_email=False):
         data = {
@@ -80,27 +105,28 @@ class User(UserMixin,db.Model):
 class Post(PaginatedAPIMixin,db.Model):
     id = db.Column(db.Integer, primary_key=True)
     post_id = db.Column(db.String(64), index=True, unique=True)
+    description = db.Column(db.String(64), index=True, unique=True)
     original_post = db.Column(db.String(5000), index=True, unique=True)
+    blog_post = db.Column(db.String(5000), index=True, unique=True)
     linkedin_post = db.Column(db.String(5000),unique=True)
     facebook_post = db.Column(db.String(5000),unique=True)
     twitter_thread = db.Column(db.String(5000),unique=True)
+    status = db.Column(db.Boolean())
     timestamp = db.Column(db.DateTime, index=True, default=datetime.now)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
         return '<User {}>'.format(self.original_post)
     
+
     def to_dict(self, include_email=False):
         data = {
             'id': self.id,
             'original_post': self.original_post,
             'timestamp': self.timestamp,
-            'user_id': self.user_id
-            # '_links': {
-            #     'self': url_for('api.get_user', id=self.id),
-            #     'avatar': self.avatar(128)
-            # }
+            'user_id': self.user_id,
+            '_links': {
+                'self': url_for('api.get_user', id=self.user_id),
+            }
         }
-        if include_email:
-            data['email'] = self.email
         return data
